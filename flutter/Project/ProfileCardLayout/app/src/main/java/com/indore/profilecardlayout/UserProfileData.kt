8db822acb5e01e4716022d73ab2b19b7458dd344
Salman Khan <38778138+SalmanBrokly@users.noreply.github.com>
2023-12-11 17:15:21 +0530

    Delete flutter/Project/ProfileCardLayout directory

--- flutter/Project/ProfileCardLayout/app/src/main/java/com/indore/profilecardlayout/UserProfileData.kt
+++
@@ -1,6 +0,0 @@
-package com.indore.profilecardlayout
-
-import android.graphics.drawable.Drawable
-import android.net.Uri
-
-data class UserProfileData(val name:String,val isOnline:Boolean, val image:Int)
